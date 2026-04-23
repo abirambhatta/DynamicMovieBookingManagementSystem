87340fb3e2e16a99d0dcaf1fc7f7c2540260814f
abirambhatta <109712943+abirambhatta@users.noreply.github.com>
2026-04-23 09:25:43 +0545

    Remove unnecessary files

--- add_last_activity_column.sql
+++
@@ -1,7 +0,0 @@
--- Add last_activity column to track user sessions
--- Run this SQL in your MySQL database
-
-ALTER TABLE users ADD COLUMN last_activity TIMESTAMP NULL DEFAULT NULL AFTER created_at;
-
--- Update existing users to have NULL last_activity (they are offline)
-UPDATE users SET last_activity = NULL;
--- remove_last_activity_column.sql
+++
@@ -1,4 +0,0 @@
--- Remove last_activity column since we're not using session tracking
--- Run this SQL in your MySQL database
-
-ALTER TABLE users DROP COLUMN last_activity;
